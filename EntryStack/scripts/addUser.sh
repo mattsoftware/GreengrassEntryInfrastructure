37bc27ac5daf1d1d9b5dc26724a85bea873ad973
Matt Paine <matt@mattsoftware.com>
2022-04-06 17:58:14 +1000

    Update adduser script with max id

--- EntryStack/scripts/addUser.sh
+++ EntryStack/scripts/addUser.sh
@@ -3,6 +3,8 @@ NAME=$1; shift
 [[ -z $NAME ]] && echo "Please provide a name to add" && exit 1
 
 TABLE_NAME=$(yarn -s output | jq '.'$(yarn -s output | jq 'keys|map(match("^.*UserTable$"))[0].string' -r) -r)
-ITEM='{"id": { "N": "'"$(yarn -s listUsers 2>/dev/null| tail -1 | echo "$(awk '{print $1}') + 1"|bc)"'"}, "enabled": {"BOOL": true}, "name": {"S": "'$NAME'"}}'
+
+CURRENT_MAX_ID=$(aws dynamodb scan --table-name "$TABLE_NAME" | jq '.Items[].id.N' -r | sort -n | tail -1)
+ITEM='{"id": { "N": "'"$(echo "$CURRENT_MAX_ID + 1"|bc)"'"}, "enabled": {"BOOL": true}, "name": {"S": "'$NAME'"}}'
 aws dynamodb put-item --table-name "$TABLE_NAME" --item "$ITEM"
 
